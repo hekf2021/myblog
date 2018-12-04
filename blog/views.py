0ee94e6230428791be89180aa6785108f3697540
hekf2021 <hekf2021@126.com>
2018-12-04 16:49:33 +0800

    修改模板位置

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
     context['blog_count']=Blog.objects.all().count()
-    return render_to_response('blog_list.html',context)
+    return render_to_response('blog/blog_list.html',context)
 
 def blog_detail(request,blog_pk):
     print(blog_pk)
@@ -14,11 +14,11 @@ def blog_detail(request,blog_pk):
     blog = get_list_or_404(Blog,pk=blog_pk)
     print(blog)
     context['blog']=blog[0]
-    return render_to_response('blog_detail.html',context)
+    return render_to_response('blog/blog_detail.html',context)
 
 def blog_with_type(request,blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType,pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
-    return render_to_response('blogs_with_type.html',context)
+    return render_to_response('blog/blogs_with_type.html',context)
