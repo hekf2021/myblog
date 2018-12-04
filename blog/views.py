ad6e037727ae2c1168692ab41ba7a73ed46e5ce1
hekf2021 <hekf2021@126.com>
2018-12-04 16:11:11 +0800

    新加功能

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response,get_list_or_404,get_object_or_404
+from .models import BlogType,Blog
 # Create your views here.
+
+def blog_list(request):
+    context = {}
+    context['blogs'] = Blog.objects.all()
+    context['blog_count']=Blog.objects.all().count()
+    return render_to_response('blog_list.html',context)
+
+def blog_detail(request,blog_pk):
+    print(blog_pk)
+    context = {}
+    blog = get_list_or_404(Blog,pk=blog_pk)
+    print(blog)
+    context['blog']=blog[0]
+    return render_to_response('blog_detail.html',context)
+
+def blog_with_type(request,blog_type_pk):
+    context = {}
+    blog_type = get_object_or_404(BlogType,pk=blog_type_pk)
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_type'] = blog_type
+    return render_to_response('blogs_with_type.html',context)
