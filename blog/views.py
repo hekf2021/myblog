f8782b77f11ee223690504be5b03ddfab6eeae5a
hekf2021 <hekf2021@126.com>
2018-12-05 22:20:24 +0800

    添加分页功能

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,15 @@ from django.core.paginator import Paginator
 def blog_list(request):
     page_num = request.GET.get('page',1)
     blog_all_list = Blog.objects.all()
-    paginator =Paginator(blog_all_list,10)
+    paginator =Paginator(blog_all_list,2)
     page_of_blogs=paginator.get_page(page_num)
+    current_page_num = page_of_blogs.number #当前页
+    page_range=list(range(max(current_page_num-2,1),current_page_num))+\
+               list(range(current_page_num,min(current_page_num+2,paginator.num_pages)+1))
 
     context = {}
     context['page_of_blogs'] =page_of_blogs
+    context['page_range'] = page_range
     context['blog_types']=BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
