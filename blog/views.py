451339de99a912546198a38b886f715c11980022
hekf2021 <hekf2021@126.com>
2018-12-05 21:45:17 +0800

    添加分页功能

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render_to_response,get_list_or_404,get_object_or_404
 from .models import BlogType,Blog
+from django.core.paginator import Paginator
 # Create your views here.
 
 def blog_list(request):
+    page_num = request.GET.get('page',1)
+    blog_all_list = Blog.objects.all()
+    paginator =Paginator(blog_all_list,10)
+    page_of_blogs=paginator.get_page(page_num)
+
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] =page_of_blogs
     context['blog_types']=BlogType.objects.all()
-    context['blog_count']=Blog.objects.all().count()
     return render_to_response('blog/blog_list.html', context)
 
 def blog_detail(request,blog_pk):
