eaf2b5e21d11209ee8de61c8ac5a6e7212f4c099
hekf2021 <hekf2021@126.com>
2018-12-09 00:01:10 +0800

    添加富文本编辑器

--- myblog/settings.py
+++ myblog/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'ckeditor',
+    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
+
+#配置media
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+#配置ckeditor 图片上传路径
+CKEDITOR_UPLOAD_PATH='upload/'
